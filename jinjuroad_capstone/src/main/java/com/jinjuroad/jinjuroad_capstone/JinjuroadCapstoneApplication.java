a2fa56d9470a9fcd7523b34e2bc9280a08b39307
Thompsonclass <thompson7219@gmail.com>
2025-10-31 11:08:31 +0900

    spring boot Project

---
+++ jinjuroad_capstone/src/main/java/com/jinjuroad/jinjuroad_capstone/JinjuroadCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.jinjuroad.jinjuroad_capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinjuroadCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinjuroadCapstoneApplication.class, args);
+	}
+
+}
---
+++ jinjuroad_capstone/src/test/java/com/jinjuroad/jinjuroad_capstone/JinjuroadCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinjuroad.jinjuroad_capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinjuroadCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
